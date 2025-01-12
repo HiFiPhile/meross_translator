ee631502ae0462bca62b681728a9c7f027963832
MasterPhi <admin@hifiphile.com>
2025-01-12 17:21:32 +0000

    Add user/password; improve logging.

--- meross_translator.py
+++ meross_translator.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import hashlib
 import logging
@@ -6,21 +6,25 @@ import random
 import time
 import json
 import argparse
+import sys
 
 from paho.mqtt import client as mqtt_client
 
-def connect_mqtt(address, port, ca=None):
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+def connect_mqtt(address, port, ca=None, username=None, password=None):
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
-            print('Connected to MQTT Broker!')
+            logging.info('Connected to MQTT Broker!')
         else:
-            print('Failed to connect, return code %d\n', rc)
+            logging.error(f'Failed to connect, return code {rc}')
 
     client = mqtt_client.Client()
     if ca is not None:
         client.tls_set(ca_certs=ca)
-    # client.username_pw_set(username, password)
+    client.username_pw_set(username, password)
     client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
     client.connect(address, port)
     return client
 
@@ -32,15 +36,15 @@ MAX_RECONNECT_DELAY = 60
 
 
 def on_disconnect(client, userdata, rc):
-    print('Disconnected with result code: %s', rc)
+    logging.error('Disconnected with result code: %s', rc)
     reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
     while reconnect_count < MAX_RECONNECT_COUNT:
-        print('Reconnecting in %d seconds...', reconnect_delay)
+        logging.info('Reconnecting in %d seconds...', reconnect_delay)
         time.sleep(reconnect_delay)
 
         try:
             client.reconnect()
-            print('Reconnected successfully!')
+            logging.info('Reconnected successfully!')
             return
         except Exception as err:
             logging.error('%s. Reconnect failed. Retrying...', err)
@@ -48,12 +52,12 @@ def on_disconnect(client, userdata, rc):
         reconnect_delay *= RECONNECT_RATE
         reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
         reconnect_count += 1
-    print('Reconnect failed after %s attempts. Exiting...', reconnect_count)
+    logging.error('Reconnect failed after %s attempts. Exiting...', reconnect_count)
 
 
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
-        print(f'Received {msg.payload.decode()}')
+        logging.info(f'Received {msg.payload.decode()}')
         try:
             cmd = json.loads(msg.payload.decode())
 
@@ -72,10 +76,10 @@ def subscribe(client: mqtt_client):
 
             send = json.dumps(data)
             topic = '/appliance/'+cmd['uuid']+'/subscribe'
-            print(f'Send {send} to {topic}')
+            logging.info(f'Send {send} to {topic}')
             client.publish(topic, send)
         except Exception as e:
-            print(e)
+            logging.error(e)
 
     client.subscribe('meross/translator')
     client.on_message = on_message
@@ -85,10 +89,12 @@ def run():
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--address', help='MQTT broker host address', type=str, required=True)
     parser.add_argument('-p', '--port', help='MQTT broker port', type=int, required=False, default=8883)
+    parser.add_argument('-u', '--user', help='MQTT user', type=str, required=False)
+    parser.add_argument('-P', '--password', help='MQTT password', type=str, required=False)
     parser.add_argument('-c', '--ca', help='CA file for TLS connection', required=False, default=None)
     args = parser.parse_args()
     
-    client = connect_mqtt(args.address, args.port, args.ca)
+    client = connect_mqtt(args.address, args.port, args.ca, args.user, args.password)
     subscribe(client)
     client.loop_forever()
 
